b1c7a015c9e69f4df62d1e88648b6fc8adc935c6
eduardosasso <eduardo.sasso@gmail.com>
2019-03-06 11:40:58 -0800

    retab

--- rails/test/services/account_config_test.rb
+++ rails/test/services/account_config_test.rb
@@ -2,17 +2,17 @@ require 'test_helper'
 
 class AccountConfigTest < ActiveSupport::TestCase
   test 'default config' do
-		config = AccountConfig.default
+    config = AccountConfig.default
 
-    assert_equal(config.theme, 'default') 
-		assert_nil(config.google_analytics)
-		assert(config.pro? == false)
+    assert_equal(config.theme, 'default')
+    assert_nil(config.google_analytics)
+    assert(config.pro? == false)
   end
 
-	test 'theme' do
-    config_yml = {'theme' => 'banana'}
+  test 'theme' do
+    config_yml = { 'theme' => 'banana' }
     config = AccountConfig.new(config_yml)
 
-    assert_equal(config.theme, 'banana') 
-	end
+    assert_equal(config.theme, 'banana')
+  end
 end
