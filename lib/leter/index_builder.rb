90504512bd47e9ca10bdf80452a6af4de2109322
eduardosasso <eduardo.sasso@gmail.com>
2019-07-18 14:27:23 -0700

    fix tab spaces

--- lib/leter/index_builder.rb
+++ lib/leter/index_builder.rb
@@ -1,54 +1,54 @@
 module Leter
-	class IndexBuilder
-		attr_reader :index
+  class IndexBuilder
+    attr_reader :index
 
-		INDEX_PARTIAL = File.read(File.expand_path('_index.html.erb', __dir__))
+    INDEX_PARTIAL = File.read(File.expand_path('_index.html.erb', __dir__))
 
-		def initialize(config = Leter::AccountConfig.default)
-			@config = config
-			@index = {}
-		end
+    def initialize(config = Leter::AccountConfig.default)
+      @config = config
+      @index = {}
+    end
 
-		def add(path, title)
-			#TODO reading time only if greater than like 2 min
+    def add(path, title)
+      #TODO reading time only if greater than like 2 min
 
-			root = Pathname(path).dirname.split.first.to_s
-			updated_at = File.mtime(path)
+      root = Pathname(path).dirname.split.first.to_s
+      updated_at = File.mtime(path)
 
-			#TODO struct or poro
-			(@index[root] ||= []) << {title: title, url: path, updated_at: updated_at}
-		end
+      #TODO struct or poro
+      (@index[root] ||= []) << {title: title, url: path, updated_at: updated_at}
+    end
 
-		def html(&block)
-			# takes a block that can be used to save the file to disk
+    def html(&block)
+      # takes a block that can be used to save the file to disk
 
-			html_template = Leter::HtmlTemplate.new.tap do |h|
-				h.theme = @config.theme
-			end
+      html_template = Leter::HtmlTemplate.new.tap do |h|
+        h.theme = @config.theme
+      end
 
-			index.each do |index_title, items|
-				html_template.tap do |h|
-					h.title = index_title
-					h.body = index_html(items) 
-				end
+      index.each do |index_title, items|
+        html_template.tap do |h|
+          h.title = index_title
+          h.body = index_html(items) 
+        end
 
-				html = ERB.new(Leter::LAYOUT).result(html_template.get_binding)
+        html = ERB.new(Leter::LAYOUT).result(html_template.get_binding)
 
-				block.call(html)
-			end
+        block.call(html)
+      end
 
-			nil
-		end
+      nil
+    end
 
-		private
+    private
 
-		def index_html(items)
-			index_template = Leter::IndexTemplate.new.tap do |index|
-				#TODO pass title to render as h1
-				index.items = items	
-			end
+    def index_html(items)
+      index_template = Leter::IndexTemplate.new.tap do |index|
+        #TODO pass title to render as h1
+        index.items = items 
+      end
 
-			ERB.new(INDEX_PARTIAL).result(index_template.get_binding)
-		end
-	end
+      ERB.new(INDEX_PARTIAL).result(index_template.get_binding)
+    end
+  end
 end
--- test/index_builder_test.rb
+++ test/index_builder_test.rb
@@ -2,43 +2,43 @@ require 'test_helper'
 require 'nokogiri'
 
 class IndexBuilderTest < Minitest::Test
-	def setup
-		Leter::IO.save_file('blog/docker-compose/index.html','#index')
-		Leter::IO.save_file('blog/vim/index.html','#vim')
+  def setup
+    Leter::IO.save_file('blog/docker-compose/index.html','#index')
+    Leter::IO.save_file('blog/vim/index.html','#vim')
 
-		Leter::IO.save_file('articles/london/index.html','#london')
-		Leter::IO.save_file('articles/paris/index.html','#paris')
-	end
+    Leter::IO.save_file('articles/london/index.html','#london')
+    Leter::IO.save_file('articles/paris/index.html','#paris')
+  end
 
-	def teardown
-		Leter::IO.delete_all('blog/', 'articles/')
-	end
+  def teardown
+    Leter::IO.delete_all('blog/', 'articles/')
+  end
 
-	def test_add
-		index_builder = Leter::IndexBuilder.new
+  def test_add
+    index_builder = Leter::IndexBuilder.new
 
-		index_builder.add('blog/docker-compose/index.html', 'Playing with Docker Compose')
-		index_builder.add('blog/vim/index.html', 'Learning Vim')
+    index_builder.add('blog/docker-compose/index.html', 'Playing with Docker Compose')
+    index_builder.add('blog/vim/index.html', 'Learning Vim')
 
-		index_builder.add('articles/london/index.html', 'Exploring London')
-		index_builder.add('articles/paris/index.html', 'Walking in Paris')
+    index_builder.add('articles/london/index.html', 'Exploring London')
+    index_builder.add('articles/paris/index.html', 'Walking in Paris')
 
-		assert_equal(['blog', 'articles'], index_builder.index.keys)
-		assert_equal(2, index_builder.index['blog'].count)
-	end
+    assert_equal(['blog', 'articles'], index_builder.index.keys)
+    assert_equal(2, index_builder.index['blog'].count)
+  end
 
-	def test_html
-		index_builder = Leter::IndexBuilder.new
+  def test_html
+    index_builder = Leter::IndexBuilder.new
 
-		# index_builder.add('blog/docker-compose/index.html', 'Playing with Docker Compose')
-		# index_builder.add('blog/vim/index.html', 'Learning Vim')
+    # index_builder.add('blog/docker-compose/index.html', 'Playing with Docker Compose')
+    # index_builder.add('blog/vim/index.html', 'Learning Vim')
 
-		index_builder.add('articles/london/index.html', 'Exploring London')
-		index_builder.add('articles/paris/index.html', 'Walking in Paris')
+    index_builder.add('articles/london/index.html', 'Exploring London')
+    index_builder.add('articles/paris/index.html', 'Walking in Paris')
 
-		index_builder.html do |h|
-			html = Nokogiri::HTML.parse(h)
-			assert_equal(['Exploring London', 'Walking in Paris'], html.css('li').collect(&:text))
-		end
-	end
+    index_builder.html do |h|
+      html = Nokogiri::HTML.parse(h)
+      assert_equal(['Exploring London', 'Walking in Paris'], html.css('li').collect(&:text))
+    end
+  end
 end
