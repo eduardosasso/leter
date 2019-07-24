e836ebbe95ab1627181cc1415ed1526c5c611d0c
eduardosasso <eduardo.sasso@gmail.com>
2019-07-24 10:17:29 -0700

    rename

---
+++ lib/leter/html.rb
@@ -0,0 +1,37 @@
+module Leter
+  class Html
+    attr_reader :html
+
+    def initialize(html)
+      @html = html
+    end
+
+    def title
+      first_h1 || first_h2
+    end
+
+    def description
+      first_h2 || first_paragraph
+    end
+
+    def first_h1
+      parser.at_css('h1').try(:text)
+    end
+
+    def first_h2
+      parser.at_css('h2').try(:text)
+    end
+
+    def first_paragraph
+      parser.at_css('p').try(:text)
+    end
+
+    def powered_by
+      parser.at('meta[name="generator"]')['content']
+    end
+
+    def parser
+      @parser ||= Nokogiri::HTML.parse(html)
+    end
+  end
+end
---
+++ test/html_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class HtmlTest < Minitest::Test
+  def test_powered_by
+    markdown = <<~MD
+      # hello world
+    MD
+
+    page_builder = Leter::PageBuilder.new(markdown)
+
+    html = page_builder.html
+
+    html_parser = Leter::Html.new(html)
+
+    assert_equal('Leter', html_parser.powered_by)
+  end
+end
