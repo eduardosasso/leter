8a7f1683caa3d304e1736f8d4c431d4364c85c28
eduardosasso <eduardo.sasso@gmail.com>
2019-08-13 09:41:47 -0700

    test ga and css

--- test/page_builder_test.rb
+++ test/page_builder_test.rb
@@ -40,4 +40,39 @@ class PageBuilderTest < Minitest::Test
 
     assert_equal(page.at('meta[name="description"]')['content'], 'first paragraph')
   end
+
+  def test_google_analytics
+    markdown = '# hello world'
+    
+    config = Leter::AccountConfig.default
+    config.google_analytics = 'UA-12345'
+
+    page_builder = Leter::PageBuilder.new(markdown, config)
+    
+    html = page_builder.html
+
+    assert_match(/ga\(\'create\', \"UA\-12345\"\, \'auto\'\)/, html)
+  end
+
+
+  def test_dev_css
+    markdown = '# hello world'
+    
+    config = Leter::AccountConfig.default
+    config.debug = true
+
+    page_builder = Leter::PageBuilder.new(markdown, config)
+    
+    assert_match(Leter::Config.css_dev_url, page_builder.html)
+  end
+
+  def test_prod_css
+    markdown = '# hello world'
+    
+    config = Leter::AccountConfig.default
+
+    page_builder = Leter::PageBuilder.new(markdown, config)
+    
+    assert_match(Leter::Config.css_prod_url, page_builder.html)
+  end
 end
